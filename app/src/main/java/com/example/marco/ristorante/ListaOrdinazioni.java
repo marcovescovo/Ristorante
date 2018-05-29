75abbfc790a947b7ac150174fb1e5f3efa57c64d
marcovescovo <marco.vescovo99@gmail.com>
2018-05-29 10:39:06 +0200

    .

--- app/src/main/java/com/example/marco/ristorante/ListaOrdinazioni.java
+++ app/src/main/java/com/example/marco/ristorante/ListaOrdinazioni.java
@@ -1,120 +1,70 @@
 package com.example.marco.ristorante;
 
-import android.content.DialogInterface;
-import android.database.Cursor;
-import android.graphics.Typeface;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.TextView;
-
-
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-import java.net.URL;
-import com.google.gson.Gson;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class ListaOrdinazioni extends AppCompatActivity {
 
     protected static ArrayList<Ordine> lOrdini=new ArrayList<>();
+    protected static String[] tipi;
+    protected static String[] ore;
+    protected static String[] specifiche;
+    protected static String[] tavoli;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_ordinazioni);
+
         try {
             aggiornaOrdinazioni();
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
 
-    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
-    public void nuovoOrdine(final Ordine o){
-        LinearLayout lista_ordinazioni=(LinearLayout) findViewById(R.id.listview_ordini);
-
-        LinearLayout ordinazione=new LinearLayout(this);
-        ordinazione.setOrientation(LinearLayout.HORIZONTAL);
-        ordinazione.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 60));
-
-        TextView tipo_ordinazione=new TextView(this);
-        tipo_ordinazione.setWidth(110);
-        tipo_ordinazione.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
-        tipo_ordinazione.setText(o.getTipo()+":");
-        tipo_ordinazione.setTextSize(15);
-        tipo_ordinazione.setTextColor(000000);
-        tipo_ordinazione.setLeft(5);
-        tipo_ordinazione.setTop(23);
-
-        TextView specifiche_ordinazione=new TextView(this);
-        specifiche_ordinazione.setWidth(120);
-        specifiche_ordinazione.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
-        specifiche_ordinazione.setText(o.getSpecifiche());
-        specifiche_ordinazione.setTextColor(000000);
-        specifiche_ordinazione.setTop(23);
-
-        TextView ora_ordinazione=new TextView(this);
-        ora_ordinazione.setWidth(50);
-        ora_ordinazione.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
-        ora_ordinazione.setText(o.getOra());
-        ora_ordinazione.setTextColor(000000);
-        ora_ordinazione.setTypeface(null, Typeface.BOLD);
-        ora_ordinazione.setTop(23);
-        ora_ordinazione.setLeft(10);
-
-        Button bottone_ordinazione=new Button(this);
-        bottone_ordinazione.setWidth(50);
-        bottone_ordinazione.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
-        bottone_ordinazione.setText(o.getTavolo());
-        bottone_ordinazione.setTop(23);
-        bottone_ordinazione.setLeft(10);
-        bottone_ordinazione.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                eliminaOrdinazione(v);
-            }
-        });
-
-        ordinazione.addView(tipo_ordinazione);
-        ordinazione.addView(specifiche_ordinazione);
-        ordinazione.addView(ora_ordinazione);
-        ordinazione.addView(bottone_ordinazione);
-        ordinazione.setOnLongClickListener(new View.OnLongClickListener(){
-            public boolean onLongClick(View arg0) {
-                creaAlert("NOTE",o.getNote());
-                return false;
-            }
-        });
+        riempiArray();
+        ListView listView=(ListView)findViewById(R.id.listviewordini);
         try{
-            lista_ordinazioni.addView(ordinazione);
+            CustomAdapter customAdapter=new CustomAdapter();
+            listView.setAdapter(customAdapter);
         }catch(Exception e){
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 
-    public void eliminaOrdinazione(View view){
-        LinearLayout ordinazione=(LinearLayout)view.getParent();
-        LinearLayout lw=(LinearLayout) findViewById(R.id.listview_ordini);
-        lw.removeView(ordinazione);
+    public static void riempiArray(){
+        tipi=new String[lOrdini.size()];
+        specifiche=new String[lOrdini.size()];
+        ore=new String[lOrdini.size()];
+        tavoli=new String[lOrdini.size()];
+        int i=0;
+        for(Ordine o:lOrdini){
+            tipi[i]=o.getTipo();
+            ore[i]=o.getOra();
+            specifiche[i]=o.getSpecifiche();
+            tavoli[i]=o.getTavolo();
+            i++;
+        }
     }
 
-    public void creaAlert(String titolo, String testo){
+    /*public void eliminaOrdinazione(View view){
+        //LinearLayout ordinazione=(LinearLayout)view.getParent();
+        ListView lw=(ListView) findViewById(R.id.listviewordini);
+        lw.removeView(view);
+    }*/
+
+    /*public void creaAlert(String titolo, String testo){
         AlertDialog.Builder builder;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
@@ -130,49 +80,46 @@ public class ListaOrdinazioni extends AppCompatActivity {
                 })
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public static String richiestaJSON(String urlString) throws ProtocolException, IOException {
-        String responsestring="";
-        URL url = new URL(urlString);
-        HttpURLConnection c = (HttpURLConnection)url.openConnection();
-        c.setRequestMethod("GET");
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
-            String str;
-            while ((str = in.readLine()) != null) {
-                responsestring += str+"\n";
-            }
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        return responsestring;
-    }
-
-    public static ArrayList<Ordine> getListaOrdini(String JSON) throws JSONException {
-
-        ArrayList<Ordine> lOrdini=new ArrayList<>();
-        JSONArray arr = new JSONArray(JSON);
-        for (int i = 0; i < arr.length(); i++){
-            Ordine ordine=new Ordine();
-            ordine.setId(arr.getJSONObject(i).getInt("id"));
-            ordine.setTipo(arr.getJSONObject(i).getString("tipo"));
-            ordine.setSpecifiche(arr.getJSONObject(i).getString("specifiche"));
-            ordine.setOra(arr.getJSONObject(i).getString("ora"));
-            ordine.setTavolo(arr.getJSONObject(i).getString("tavolo"));
-            ordine.setNote(arr.getJSONObject(i).getString("note"));
-            lOrdini.add(ordine);
-        }
-        return lOrdini;
-    }
+    }*/
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void aggiornaOrdinazioni() throws IOException, JSONException {
         RunnableGET rg=new RunnableGET();
         new Thread(rg).start();
-        for(Ordine ordine:lOrdini){
-            nuovoOrdine(ordine);
+    }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return lOrdini.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView=getLayoutInflater().inflate(R.layout.customlayout,null);
+
+            TextView text_tipo=(TextView)convertView.findViewById(R.id.textView_tipo);
+            TextView text_specifiche=(TextView)convertView.findViewById(R.id.textView_specifiche);
+            TextView text_ora=(TextView)convertView.findViewById(R.id.textView_ora);
+            Button button_bottone=(Button)convertView.findViewById(R.id.button_delete);
+
+            text_tipo.setText(tipi[position]);
+            text_specifiche.setText(specifiche[position]);
+            text_ora.setText(ore[position]);
+            button_bottone.setText(tavoli[position]);
+
+            return null;
         }
     }
 }
